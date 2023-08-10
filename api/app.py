8af9d790f90e35d30f2d76f9c2d4af2cec9bfaf8
Mattwmaster58 <mattwmaster58@gmail.com>
2023-08-09 22:18:14 -0400

    ft: add logging

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 from pathlib import Path
 from typing import AsyncGenerator
@@ -5,43 +6,17 @@ from typing import AsyncGenerator
 from litestar import Litestar
 from litestar.config.compression import CompressionConfig
 from litestar.config.cors import CORSConfig
-from litestar.contrib.sqlalchemy.plugins import SQLAlchemySerializationPlugin
+from litestar.contrib.sqlalchemy.plugins import SQLAlchemySerializationPlugin, SQLAlchemyPlugin, SQLAlchemyAsyncConfig
 from litestar.datastructures import State
 from litestar.exceptions import ClientException
 from litestar.status_codes import HTTP_409_CONFLICT
 from sqlalchemy import ext
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from litestar.logging import LoggingConfig
 
 from data import Base, create_fts_table_and_triggers, AuctionLot
 from routes.data.search import search
-
-
-@asynccontextmanager
-async def db_connection(_app: Litestar) -> AsyncGenerator[None, None]:
-    engine = getattr(_app.state, "engine", None)
-    if engine is None:
-        db_path = Path(__file__).parent / "mac.bid.db"
-        engine = create_async_engine(
-            f"sqlite+aiosqlite:///{db_path.as_posix()}", echo=True
-        )
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-            fts_statements = create_fts_table_and_triggers(
-                table=AuctionLot.__table__,
-                fts_cols={AuctionLot.product_name, AuctionLot.title, AuctionLot.description},
-                version=5
-            )
-            # sqlite doesn't allow multiple statements per execute, so use the underlying connections executescript API
-            # noinspection PyUnresolvedReferences
-            raw_conn = (await conn.get_raw_connection())
-            await raw_conn.connection._connection.executescript(str(fts_statements))
-            print("created FTS mirror")
-        _app.state.engine = engine
-    try:
-        yield
-    finally:
-        await engine.dispose()
 
 
 async def provide_db(state: State) -> AsyncGenerator[ext.asyncio.engine, None]:
@@ -63,13 +38,42 @@ async def provide_transaction(state: State) -> AsyncGenerator[AsyncSession, None
                 detail=str(exc),
             ) from exc
 
+
+async def _init_db(_app: Litestar) -> None:
+    async with _app.state.db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        fts_statements = create_fts_table_and_triggers(
+            table=AuctionLot.__table__,
+            fts_cols={AuctionLot.product_name, AuctionLot.title, AuctionLot.description},
+            version=5,
+        )
+        # sqlite doesn't allow multiple statements per execute, so use the underlying connections executescript API
+        # noinspection PyUnresolvedReferences
+        raw_conn = await conn.get_raw_connection()
+        await raw_conn.connection._connection.executescript(str(fts_statements))
+
+
+db_path = Path(__file__).parent / "mac.bid.db"
+config = SQLAlchemyAsyncConfig(connection_string=f"sqlite+aiosqlite:///{db_path.as_posix()}")
+plugin = SQLAlchemyPlugin(config=config)
 cors_config = CORSConfig(allow_origins=["http://localhost:3000"])
+# logging_config = StructLoggingConfig()
+
+logging_config = LoggingConfig(
+    loggers={
+        "MacFlashback": {
+            "level": "DEBUG",
+            "handlers": ["queue_listener"],
+        }
+    }
+)
 app = Litestar(
     route_handlers=[search],
     dependencies={"tx": provide_transaction, "db": provide_db},
-    lifespan=[db_connection],
-    plugins=[SQLAlchemySerializationPlugin()],
+    plugins=[SQLAlchemyPlugin(config=config), SQLAlchemySerializationPlugin()],
     cors_config=cors_config,
     compression_config=CompressionConfig(backend="brotli"),
-    debug=True
+    on_startup=[_init_db],
+    logging_config=logging_config,
+    # debug=True,  # required until this is fixed: https://github.com/litestar-org/litestar/issues/1804
 )
