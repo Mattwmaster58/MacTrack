793c422fd3b9d8ddd15f9da6928f712c7bf62da1
Mattwmaster58 <mattwmaster58@gmail.com>
2023-08-14 00:15:19 -0400

    chore: inline unreusable components due to type annoyanness

--- frontend/src/components/forms/itemFilterForm/itemFilterForm.tsx
+++ frontend/src/components/forms/itemFilterForm/itemFilterForm.tsx
@@ -1,16 +1,20 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, FormControl, FormControlLabel, Switch } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  MenuItem,
+  Select,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Stack } from "@mui/system";
 import React from "react";
 import { Controller, FormProvider, useForm } from "react-hook-form";
 import { FilterMatchType } from "../../../types/FilterMatchType";
-
-import { RetailPriceInput } from "./retailPriceInput";
-import { TermsInput } from "./termsInput";
-import {
-  ItemFilterOutputValues,
-  ItemFilterInputValues, 
-  ItemFilterSchema,
-} from './types/itemFilter'
+import { TagInput } from "../../elements/tagInput";
+import { ItemFilterInputValues, ItemFilterSchema } from "./types/itemFilter";
 
 interface Props {
   onSubmit: (data: ItemFilterInputValues) => void;
@@ -42,8 +46,67 @@ const ItemFilterForm = ({ onSubmit }: Props) => {
     <FormControl>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormProvider {...methods}>
-          {TermsInput(control)}
-          {RetailPriceInput(control)}
+          <Stack flexDirection="column" spacing={2}>
+            <Stack flexDirection="column" spacing={2}>
+              <Stack flexDirection="row" alignItems="center" spacing={2}>
+                <Typography alignItems={"center"}>Include items
+                  where</Typography>
+                <Controller
+                  control={control}
+                  name="fts_query.boolean_function"
+                  render={({ field }) => <Select {...field}>
+                      <MenuItem value={FilterMatchType.ANY}>Any</MenuItem>
+                      <MenuItem value={FilterMatchType.ALL}>All</MenuItem>
+                    </Select>}
+                />
+                of the following terms match
+              </Stack>
+              <Controller
+                name="fts_query.includes"
+                control={control}
+                render={({ field: { onChange, value } }) => <TagInput onTagsChange={onChange} value={value}/>}
+              />
+            </Stack>
+            <Stack flexDirection="row" alignItems="center" spacing={2}>
+              <Typography alignItems={"center"}>
+                and exclude those that contain any of the following
+              </Typography>
+              <Controller
+                name="fts_query.excludes"
+                control={control}
+                render={({ field: { onChange, value } }) => <TagInput onTagsChange={onChange} value={value}/>}
+              />
+            </Stack>
+          </Stack>
+          <Stack>
+            <Typography>Retail price</Typography>
+            <Stack flexDirection="row" spacing={2}>
+              <Controller
+                defaultValue={""}
+                name="min_retail_price"
+                control={control}
+                render={({ field }) => <TextField
+                    label="Minimum"
+                    error={!!errors.min_retail_price?.message}
+                    helperText={errors.min_retail_price?.message ?? "\u00a0"}
+                    inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                    {...field}
+                  />}
+              />
+              <Controller
+                defaultValue={""}
+                name="max_retail_price"
+                control={control}
+                render={({ field }) => <TextField
+                    label="Maximum"
+                    error={!!errors.max_retail_price?.message}
+                    helperText={errors.max_retail_price?.message ?? "\u00a0"}
+                    inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                    {...field}
+                  />}
+              />
+            </Stack>
+          </Stack>
           <Controller
             name="fts_query.columns"
             control={control}
