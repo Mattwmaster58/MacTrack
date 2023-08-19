653f5955d677171e18af07d897f5af2d2697aa88
Mattwmaster58 <mattwmaster58@gmail.com>
2023-08-19 13:05:34 -0400

    ft: add name for filter

--- api/data/user/filter.py
+++ api/data/user/filter.py
@@ -9,58 +9,25 @@ Useful documentation:
  - https://www.sqlite.org/fts5.html
  - https://docs.sqlalchemy.org/en/20/core/sqlelement.html#sqlalchemy.sql.expression.bindparam
 """
-from enum import Enum
-from typing import Literal
 
-from pydantic import BaseModel
 from sqlalchemy import Dialect
 from sqlalchemy.types import TypeDecorator, String
 
-
-class BooleanFunction(str, Enum):
-    AND = "AND"
-    OR = "OR"
-
-
-Conditions = Literal["LIKE NEW", "DAMAGED", "OPEN BOX"]
-
-
-class SimpleFtsQuery(BaseModel):
-    """
-    FTS supports a grammar that is much too complex to fully support, at least for my skill level
-    This is a much less complex approximation that I suspect will serve the majority of use cases
-    """
-
-    boolean_function: BooleanFunction
-    columns: list[str]
-    includes: list[str]
-    excludes: list[str] = []
-
-    def serialize_match(self) -> str:
-        pass
-
-
-class FilterQuery(BaseModel):
-    fts_query: SimpleFtsQuery
-    min_retail_price: float = -1
-    max_retail_price: float = -1
-    damaged: bool
-    new_: bool
-    open_box: bool
+from data.http_models.filter import FilterCore
 
 
 class FilterQueryDbType(TypeDecorator):
     impl = String
     cache_ok = True
 
-    def process_bind_param(self, value: FilterQuery, _: Dialect) -> str | None:
+    def process_bind_param(self, value: FilterCore, _: Dialect) -> str | None:
         if value is None:
             return None
-        if not isinstance(value, FilterQuery):
-            raise TypeError(f"expected argument of type FilterQuery, got {FilterQuery}")
+        if not isinstance(value, FilterCore):
+            raise TypeError(f"expected argument of type FilterQuery, got {FilterCore}")
         return value.model_dump_json(exclude_none=True)
 
-    def process_result_value(self, value: str | None, _: Dialect) -> FilterQuery | None:
+    def process_result_value(self, value: str | None, _: Dialect) -> FilterCore | None:
         if value is None:
             return None
-        return FilterQuery.model_validate_json(value)
+        return FilterCore.model_validate_json(value)
