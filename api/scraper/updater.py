9bed08cbe83256bd2a8963a7e2ebffe7a9c9f04c
Mattwmaster58 <mattwmaster58@gmail.com>
2023-08-23 11:46:04 -0400

    ft: refactor cleanup operations + new cleanup op: adding closed_date to lots whose created_date was long ago

--- api/scraper/updater.py
+++ api/scraper/updater.py
@@ -1,11 +1,13 @@
 import asyncio
 import time
+from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import Iterable
+from typing import Iterable, Type
 
-from sqlalchemy import false, update, select, join, func, insert, Select
+from sqlalchemy import false, select, join, func, insert, Select, update
 from tqdm import tqdm
 
+from data import Base
 from data.mac_bid import Building, Location, AuctionGroup, AuctionLot
 from scraper.api_client import ApiClient
 from scraper.utils import batched
@@ -169,45 +171,66 @@ class MacBidUpdater:
         """
         Core parts of the scraping logic depend on AuctionGroup.is_active being accurate, but this is not always the
         case, so we attempt to fix things up here with a few different approaches
-            - First, close any lots that are past the close date
-            - Close any groups that have all lots closed
+            - Add a closed_date to any lots that are CREATED_DATE_TO_CLOSED_DAYS_DELTA_THRESHOLD past the created date
+        We use this query to find some stats on the avg deltas to pick CREATED_DATE_TO_CLOSED_DAYS_DELTA_THRESHOLD
+        
+        SELECT MIN(delta), MAX(delta), AVG(delta), MEDIAN(delta)
+        FROM (SELECT JULIANDAY(a.closed_date) - JULIANDAY(a.date_created) delta
+          FROM auctionlot a
+          WHERE a.closed_date IS NOT NULL
+            AND a.date_created IS NOT NULL
+        AND (julianday(date('now')) - julianday(a.date_created)) < 365)
+        
+        Current results: min/man/avg/median = -27.63/32.31/3.54/4.13
+        
+            - Close any lots that are past the closed_date
+            - Close any groups that have all lots closed - todo: should we do the inverse of this as well?
             - Close any groups that have a date_completed in the past
             - Close any groups that have abandon in the past
         """
+        @dataclass
+        class CleanupOp:
+            table: Type[Base]
+            description: str
+            and_clauses: list
+            static_values: dict[str, any]
+
         now = datetime.now()
-        close_lots_where_closed_date_is_passed = (
-            update(AuctionLot).where(AuctionLot.is_open & (now > AuctionLot.closed_date)).values(is_open=False)
-        )
+        CREATED_DATE_TO_CLOSED_DAYS_DELTA_THRESHOLD = 14
+        CREATED_DATE_TO_CLOSED_DAYS_TIMEDELTA_THRESHOLD = timedelta(days=CREATED_DATE_TO_CLOSED_DAYS_DELTA_THRESHOLD)
 
-        close_groups_where_all_lots_are_closed = (
-            update(AuctionGroup)
-            .where(AuctionGroup.id ==
-                   select(AuctionGroup.id)
-                   .select_from(join(AuctionGroup, AuctionLot, AuctionGroup.id == AuctionLot.auction_id))
-                   .where(AuctionGroup.is_open)
-                   .group_by(AuctionGroup.id)
-                   .having(func.count(1).filter(AuctionLot.is_open) == 0)
-                   .scalar_subquery()
-                   )
-            .values(is_open=False)
-        )
-
-        close_groups_if_date_completed_or_abandoned_past = (
-            update(AuctionGroup)
-            .where(AuctionGroup.is_open & ((now > AuctionGroup.closing_date) | (now > AuctionGroup.abandon_date)))
-            .values(is_open=False)
-        )
-
-        step_explanations = [
-            (close_lots_where_closed_date_is_passed, "closing lots where closed_date is passed"),
-            (close_groups_where_all_lots_are_closed, "closing groups where all lots within are closed"),
-            (
-                close_groups_if_date_completed_or_abandoned_past,
-                "closing groups where date completed/abandoned is in the past",
-            ),
-        ]
-        for stmt, explanation in step_explanations:
+        all_cleanup_ops = [
+            CleanupOp(
+                table=AuctionLot,
+                description="adding closed_date to lots whose created_date was long ago",
+                and_clauses=[AuctionLot.is_open,
+                             AuctionLot.closed_date == None,
+                             AuctionLot.date_created != None,
+                             AuctionLot.date_created < now - CREATED_DATE_TO_CLOSED_DAYS_TIMEDELTA_THRESHOLD],
+                static_values={"closed_date": now}
+            ), CleanupOp(
+                table=AuctionLot,
+                description="closing lots where closed_date is passed",
+                and_clauses=[AuctionLot.is_open, now > AuctionLot.closed_date],
+                static_values={"is_open": False}
+            ), CleanupOp(
+                table=AuctionGroup,
+                description="closing groups where all lots within are closed",
+                and_clauses=[AuctionGroup.id == select(AuctionGroup.id).select_from(
+                    join(AuctionGroup, AuctionLot, AuctionGroup.id == AuctionLot.auction_id)).where(
+                    AuctionGroup.is_open).group_by(AuctionGroup.id).having(
+                    func.count(1).filter(AuctionLot.is_open) == 0).scalar_subquery()],
+                static_values={"is_open": False}
+            ), CleanupOp(
+                table=AuctionGroup,
+                description="closing groups where date completed/abandoned is in the past",
+                and_clauses=[AuctionGroup.is_open,
+                             (now > AuctionGroup.closing_date) | (now > AuctionGroup.abandon_date)],
+                static_values={"is_open": False}
+            )]
+        for op in all_cleanup_ops:
             start = time.perf_counter()
-            res = await self.session.execute(stmt)
-            print(f"{explanation} resulted in {res.rowcount} rows effected in {time.perf_counter() - start:.2f}s")
+            update_stmt = update(op.table).where(*op.and_clauses).values(**op.static_values)
+            res = await self.session.execute(update_stmt)
+            print(f"{op.description} resulted in {res.rowcount} rows effected in {time.perf_counter() - start:.2f}s")
         await self.session.commit()
