6481339ae40473779b56b3ad44bb08ee098bbede
Mattwmaster58 <mattwmaster58@gmail.com>
2023-08-23 14:55:01 -0400

    fix: extract cleanup module

--- api/cli.py
+++ api/cli.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import click
 
 from database import init_db_from_engine, create_async_db_config
+from scraper.cleanup import cleanup
 from scraper.updater import MacBidUpdater
 
 
@@ -25,11 +26,11 @@ async def session_from_db_path(db_path: Path):
     return session
 
 
-async def _update(db_path: Path, skip_correction: bool):
+async def _update(db_path: Path, skip_cleanup: bool):
     session = await session_from_db_path(db_path)
     mbc = MacBidUpdater(session)
-    if not skip_correction:
-        await mbc.correct_auction_groups()
+    if not skip_cleanup:
+        await cleanup(mbc.session)
     await mbc.update_locations()
     await mbc.update_buildings()
     # todo: sanity checks: how many lots were open before/after scrape
@@ -40,10 +41,10 @@ async def _update(db_path: Path, skip_correction: bool):
 
 @click.command()
 @click.option("--db-path", default=Path(__file__).parent / "mac.bid.db", type=click.Path())
-@click.option("--skip-correction", default=False, is_flag=True)
-def update(db_path: Path, skip_correction: bool):
+@click.option("--skip-cleanup", default=False, is_flag=True)
+def update(db_path: Path, skip_cleanup: bool):
     db_path = Path(db_path).absolute()
-    asyncio.run(_update(db_path, skip_correction))
+    asyncio.run(_update(db_path, skip_cleanup))
 
 
 if __name__ == '__main__':
